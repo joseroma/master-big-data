ae5ce208b1684fceedebb63f51f06f71ec01b97e
joseroma <josrodmal0@gmail.com>
2021-01-29 21:33:36 +0100

    Update MASTER

---
+++ 4_fundamentos_nlp/ejercicio_final/main.py
@@ -0,0 +1,7 @@
+#############################################################################
+#
+#       Ejercicio sobre extracción de información (Information Extraction)
+#
+# Este ejercicio también incluye una gran cantidad de elementos que previamente han sido estudiados
+# (NLTK, cadenas NLP, entrenamiento de taggers para el castellano, tokenización, uso de corpus de entrenamiento).
+#
